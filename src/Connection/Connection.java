5ff1f1a701458d53091ba12645a94ba5ffeb5795
sukma <ASUS@LAPTOP-MDQ1DD3E>
2019-06-30 22:07:22 +0700

    modifikasi class connection

--- src/Connection/Connection.java
+++ src/Connection/Connection.java
@@ -5,10 +5,34 @@
  */
 package Connection;
 
+import java.beans.Statement;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 /**
  *
  * @author ASUS
  */
 public class Connection {
+    Connection con;
+    Statement st;
+    ResultSet rs;
+    String pesan;
+    PreparedStatement ps;
+
+    public Connection() {
+        pesan ="";
+        try{
+            Class.forName("org.postgresql.Driver");
+            con = (Connection) DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/projectpbo","pbo","pbo123");
+            pesan="SUKSES";
+            
+        }catch (Exception e){
+           pesan = e.getMessage();
+        }
+    }
+    
     
 }
